02e89cf2496fcb334a638474567c11c0ecf5b85b
marleigh <volcana@gmail.com>
2021-07-07 15:43:26 -0700

    basic component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 
+function App() {
+    return (
+        <div>
+            <h1>hello world</h1>
+            <p>this is me</p>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import App from './App';
 
-ReactDOM.render(<h1>hello world</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
