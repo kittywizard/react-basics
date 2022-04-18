d173babbfdb26351e625a6a6899463f39f275ede
marleigh <volcana@gmail.com>
2022-04-18 15:07:00 -0700

    basic structure and one form

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,9 +1,41 @@
-
+import React, {useState} from "react"
 
 function App() {
-  return (
-    <div className="App">
 
+  //create state for text area content
+  const [textarea, setTextarea] = useState("");
+
+  function handleChange(e) {
+    setTextarea(e.target.value);
+  }
+
+  function calculateWordCount(text) {
+    //string method to look for spaces
+    const strArray = text.trim().split(" ");
+    //bugs:
+      //it counts each space as a new word
+
+    console.log(strArray.length)
+    return strArray.length;
+  }
+
+  return (
+    <div className="container">
+        <h1>Speed Typing Game</h1>
+        <p className="paragraph">How fast can you type?</p>
+        <textarea 
+            onChange={handleChange}
+            name="textarea"
+            value={textarea}
+        />
+
+        <h4>Time Remaining: </h4>
+        <button 
+          onClick={() => calculateWordCount(textarea)}
+        >
+          Start Game
+        </button>
+        <h1>Word Count: </h1>
     </div>
   );
 }
