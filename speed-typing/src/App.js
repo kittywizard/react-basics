1a1bc67f5173e60511cf0d98a6d854d6180b570e
marleigh <volcana@gmail.com>
2022-04-18 18:03:48 -0700

    speed typing basic product complete

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,23 +1,45 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 
 function App() {
 
-  //create state for text area content
+  const startingTime = 10;
+
+
+  //create state
   const [textarea, setTextarea] = useState("");
+  const [countdown, setCountdown] = useState(startingTime);
+  const [startTimer, setStartTimer] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
+
+  useEffect(() => {
+    if(startTimer){
+      setTimeout(() => countdown > 0 && setCountdown(prevState => (prevState - 1)), 1000);
+    }
+
+    if(startTimer && countdown === 0) {
+      setStartTimer(prevState => (!prevState));
+      setWordCount(calculateWordCount(textarea));
+    }
+  },[countdown, startTimer]);
 
   function handleChange(e) {
     setTextarea(e.target.value);
   }
 
   function calculateWordCount(text) {
-    //string method to look for spaces
     const strArray = text.trim().split(" ");
-    //bugs:
-      //it counts each space as a new word
-
-    console.log(strArray.length)
-    return strArray.length;
+    return strArray.filter(word => word !== "").length;
   }
+
+  function resetGame() {
+    setCountdown(startingTime);
+    setWordCount(0);
+    setTextarea("");
+  }
+
+  const styles = {
+    backgroundColor: startTimer ? "#cccccc" : "#00b800"
+}
 
   return (
     <div className="container">
@@ -27,15 +49,21 @@ function App() {
             onChange={handleChange}
             name="textarea"
             value={textarea}
+            disabled={!startTimer}
         />
 
-        <h4>Time Remaining: </h4>
-        <button 
-          onClick={() => calculateWordCount(textarea)}
+        <h4>Time Remaining: {countdown}</h4>
+        <button
+          style={styles} 
+          onClick={() => {
+            countdown === 0 && resetGame()
+            setStartTimer(prevState => (!prevState))
+          }}
+          disabled={startTimer}
         >
           Start Game
         </button>
-        <h1>Word Count: </h1>
+        <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
